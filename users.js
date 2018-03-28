787702bac8f7e1c85ae24b18cc382a5436e35d96
jKiler <33574230+jKiler@users.noreply.github.com>
2018-03-28 14:31:49 +0200

    update #1

--- users.js
+++ users.js
@@ -1,51 +1,52 @@
 const express = require('express');
+const _ = require('lodash');
 const router = express.Router();
 
 const users = [
-  {id: 1, firstName: 'Marek', secondName: 'Chmiel', age: 28},
-  {id: 2, firstName: 'Jerzy', secondName: 'Kwiatkowski', age: 25}
+  {id: 1, firstName: 'Marek', secondName: 'Chmiel', age: '28'},
+  {id: 2, firstName: 'Jerzy', secondName: 'Kwiatkowski', age: '25'}
 ];
+
+const response = (res, obj) => {
+  res.header(obj.header);
+  res.status(obj.status);
+  res.json(obj.json);
+};
 
 //GET requests
 router.get('/', (req, res) => {
-  res.header('content-type', 'application/json');
-  res.status(200);
-  res.json(users);
+  const { id, firstName, secondName, age } = req.query;
+  const queryOptions = {
+    id,
+    firstName,
+    secondName,
+    age
+  };
+  const filteredUsers = _.filter(
+    users, 
+    _.pickBy(queryOptions, v => v) //since there is need to get only truthy properties from queryOptions object
+  );
+  response(res, {
+    header: "'content-type', 'application/json'",
+    status: 200,
+    json: { users: filteredUsers }
+  });
 });
 
 router.get('/:id([0-9]{1,})', (req, res) => {
-  const currUser = users.filter((user) => {
-    if(user.id == req.params.id) {
-      return true;
-    }
-  });
+  const currUser = _.find(users, [id, req.params.id]);
   if(currUser.length == 1) {
-    res.header('content-type', 'application/json');
-    res.status(200);
-    res.json(currUser[0]);
+    response(res, {
+      header: "'content-type', 'application/json'",
+      status: 200,
+      json: currUser[0],
+    });
   } else {
-    res.header('content-type', 'application/json');
-    res.status(404);
-    res.json({ message: 'Not Found' });
-  }
-});
-
-router.get('/search', (req, res) => {
-  const currUser = users.filter((user) => {
-    const queryName = Object.keys(req.query)[0];
-    const queryValue = req.query[queryName];
-    if(user[queryName] == queryValue) {
-      return true;
-    }
-  });
-  if(currUser.length == 1) {
-    res.header('content-type', 'application/json');
-    res.status(200);
-    res.json(currUser[0]);
-  } else {
-    res.header('content-type', 'application/json');
-    res.status(404);
-    res.json({ message: 'Not Found' });
+    response(res, {
+      header: "'content-type', 'application/json'",
+      status: 404,
+      json: { message: 'User was not found.' },
+    });
   }
 });
 
@@ -57,21 +58,25 @@ router.post('/', (req, res) => {
     !secondName ||
     !age.toString().match(/^[0-9]{2}$/g)
   ) {
-    res.header('content-type', 'application/json');
-    res.status(400);
-    res.json({ message: 'Bad Request' });
+    response(res, {
+      header: "'content-type', 'application/json'",
+      status: 400,
+      json: { message: 'Bad Request' },
+    });
   } else {
     const newId = users[users.length-1].id+1;
     users.push({
       id: newId,
-      firstName: firstName,
-      secondName: secondName,
-      age: parseInt(age)
+      firstName,
+      secondName,
+      age
     });
     res.location('http://localhost:3000/api/users/' + newId);
-    res.header('content-type', 'application/json');
-    res.status(201);
-    res.json({ message: 'New user created.', location: '/api/users/' + newId });
+    response(res, {
+      header: "'content-type', 'application/json'",
+      status: 201,
+      json: { message: 'New user created.', location: '/api/users/' + newId },
+    });
   }
 });
 
@@ -85,44 +90,47 @@ router.put('/:id', (req, res) => {
     !age.toString().match(/^[0-9]{2}$/g) ||
     !id.toString().match(/^[0-9]{1,}$/g)
   ) {
-    res.header('content-type', 'application/json');
-    res.status(400);
-    res.json({ message: 'Bad Request' });
+    response(res, {
+      header: "'content-type', 'application/json'",
+      status: 400,
+      json: { message: 'Bad Request' },
+    });
   } else {
-    const updateId = users.map((user) => user.id).indexOf(parseInt(id));
-    if(updateId === -1) {
-      users.push({
-        id: parseInt(id),
-        firstName: firstName,
-        secondName: secondName,
-        age: parseInt(age)
-      });
-      res.header('content-type', 'application/json');
+    const selectedId = _.findIndex(users, (user) => user.id == id);
+    const userObj = {
+      id,
+      firstName,
+      secondName,
+      age
+    };
+    if(selectedId === -1) {
+      users.push(userObj);
       res.location('http://localhost:3000/api/users/' + id);
-      res.status(201);
-      res.json({ message: 'New user created.', location: '/api/users/' + id });
+      response(res, {
+        header: "'content-type', 'application/json'",
+        status: 201,
+        json: { message: 'New user created.', location: '/api/users/' + id },
+      });
     } else {
-      users[updateId] = {
-        id: parseInt(id),
-        firstName: firstName,
-        secondName: secondName,
-        age: parseInt(age)
-      };
-      res.header('content-type', 'application/json');
-      res.status(200);
-      res.json({ message: 'User id ' + id + ' updated.', location: '/api/users/' + id });
+      users[selectedId] = userObj;
+      response(res, {
+        header: "'content-type', 'application/json'",
+        status: 200,
+        json: { message: 'User id ' + id + ' updated.'},
+      });
     }
   }
 });
 
 //DELETE request
 router.delete('/:id', (req, res) => {
-  const removeIndex = users.map((user) => user.id).indexOf(parseInt(req.params.id));
-  if(removeIndex === -1) {
+  const { id } = req.params;
+  const selectedId = _.findIndex(users, (user) => user.id == id);
+  if(selectedId === -1) {
     res.status(404);
     res.send(null);
   } else {
-    users.splice(removeIndex, 1);
+    users.splice(selectedId, 1);
     res.status(204);
     res.send(null);
   }
